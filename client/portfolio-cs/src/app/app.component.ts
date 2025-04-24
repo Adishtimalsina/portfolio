5a50b4f05fe9e35e7ccd991a06a6f094ebe4ae3e
Adish Timalsina <adishtimalsina11@gmail.com>
2025-04-23 21:54:05 -0500

    header menu fixed, and hero section added

--- client/portfolio-cs/src/app/app.component.ts
+++ client/portfolio-cs/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThreeDComponent } from "./three-d/three-d.component";
 import { NavbarComponent } from "./navbar/navbar.component"
+import {HeroSectionComponent} from "./hero-section/hero-section.component";
 
 @Component({
     selector: 'app-root',
   imports: [
-    ThreeDComponent, NavbarComponent
+    ThreeDComponent, NavbarComponent, HeroSectionComponent
   ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
--- client/portfolio-cs/src/app/constants/index.ts
+++ client/portfolio-cs/src/app/constants/index.ts
@@ -12,7 +12,7 @@ export const links = [
   },
   {
     id:3,
-    name:"Professional Experience",
+    name:"Experience",
     href:"/professionalexperience"
   },
   {
