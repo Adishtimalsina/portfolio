1b794beceb299b7acc68246bc13344fe825f2eea
Adish Timalsina <adishtimalsina11@gmail.com>
2025-05-10 10:35:24 -0500

    feat(initial-branch): made some change

--- client/portfolio-cs/src/app/three-d/three-d.component.ts
+++ client/portfolio-cs/src/app/three-d/three-d.component.ts
@@ -9,6 +9,7 @@ import {
 import * as THREE from 'three';
 import { isPlatformBrowser } from '@angular/common';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-three-d',
@@ -35,34 +36,39 @@ export class ThreeDComponent implements AfterViewInit {
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
 
-    const geometry = new THREE.SphereGeometry();
-    const plane = new THREE.PlaneGeometry(5,5);
-    const planeMaterial = new THREE.MeshStandardMaterial({color:'gray', side:THREE.DoubleSide})
-    const material = new THREE.MeshStandardMaterial({ color: 'pink' });
-    const cube = new THREE.Mesh(geometry, material);
-    const base = new THREE.Mesh(plane, planeMaterial)
-    base.rotation.x = -Math.PI/2;
-    base.position.y = -1;
-    scene.add(cube, base);
+    // const geometry = new THREE.SphereGeometry();
+    // const plane = new THREE.PlaneGeometry(5,5);
+    // const planeMaterial = new THREE.MeshStandardMaterial({color:'gray', side:THREE.DoubleSide})
+    // const material = new THREE.MeshStandardMaterial({ color: 'pink' });
+    // const cube = new THREE.Mesh(geometry, material);
+    // const base = new THREE.Mesh(plane, planeMaterial)
+    // base.rotation.x = -Math.PI/2;
+    // base.position.y = -1;
+    // scene.add(cube, base);
 
 
     const light = new THREE.AmbientLight(0xffffff, 1);
     light.position.set(1, 1, 1);
     scene.add(light);
 
-    //const orbitController = new OrbitControls(camera, renderer.domElement);
+    const orbitController = new OrbitControls(camera, renderer.domElement);
 
     camera.position.z = 9;
 
     const animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-    //  orbitController.update();
+      orbitController.update();
       renderer.render(scene, camera);
     };
 
+    // const load3D =function() {
+    //   const loader = new GLTFLoader();
+    //    loader.load('./assets/3D-assets/futuristic_room/scene.gltf', (gltf)=>{
+    //      scene.add(gltf.scene)
+    //    }, undefined, (error)=>{
+    //      console.log("error loading gltf", error)
+    //    })
+    // }
     window.addEventListener('resize', ()=>{
       const width = window.innerWidth;
       let factor = 1;
@@ -73,13 +79,13 @@ export class ThreeDComponent implements AfterViewInit {
         factor = 0.8
       }
 
-      cube.scale.set(factor, factor, factor)
-      base.scale.set(factor, factor, factor)
       camera.aspect = window.innerWidth/ window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     })
 
+    // load3D();
     animate();
+
   }
 }
